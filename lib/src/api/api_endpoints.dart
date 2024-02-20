88ddf2ccccd8f6301bb043ecd78394267886192f
smit monpara <monparasmit1@gmail.com>
2024-02-20 22:47:26 +0530

    Change:- api base url

--- lib/src/api/api_endpoints.dart
+++ lib/src/api/api_endpoints.dart
@@ -1,5 +1,5 @@
 class ApiEndpoints {
-  static const String baseUrl = 'http://13.126.139.19:80/api';
+  static const String baseUrl = 'http://13.126.139.19:5000/api';
   static const String employeeLogin = '/employee-login';
   static const String profile = '/profile';
 }
